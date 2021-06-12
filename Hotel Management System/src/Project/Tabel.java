6cb31ecb21e48580120a32c1f4481aff26dd4269
Muhammad Abel Jollando <72371489+AbelJollando@users.noreply.github.com>
2021-06-12 13:37:09 +0800

    Add files via upload

--- Hotel Management System/src/Project/Tabel.java
+++ Hotel Management System/src/Project/Tabel.java
@@ -27,8 +27,9 @@ public class Tabel{
             new SQL syntax to make the new tables
             */
 
-            //st.executeUpdate("create table admin(name varchar(50) not null, username varchar(50) not null, email varchar(50) not null, password varchar(50) not null, securityQuestion varchar(100) not null, answer varchar(100) not null, address varchar(100) not null, status varchar(20) not null)");
-            st.executeUpdate("create table room(roomNumber INT(11), roomType varchar(100), bed varchar(100), price varchar(20), status varchar(20))");
+            st.executeUpdate("create table admin(name varchar(50) not null, username varchar(50) not null, email varchar(50) not null, password varchar(50) not null, securityQuestion varchar(100) not null, answer varchar(100) not null, address varchar(100) not null, status varchar(20) not null)");
+            st.executeUpdate("create table room(roomNumber INT(11), roomType varchar(100), bed varchar(100), price varchar(20), feature varchar (200), status varchar(20), maintenance_start date, maintenance_end date)");
+            st.executeUpdate("create table customer(id int(11) primary key not null, fullName varchar(100) not null, phoneNumber varchar(13) not null, nationality varchar(50) not null, gender varchar(20) not null, idProof int (11) not null, address varchar(100) not null, checkInDate date, roomNo varchar(10) not null, bedType varchar(100) not null, roomType varchar(100) not null, pricePerDay varchar(20) not null, numberOfDays int (10), totalAmount varchar(200), checkOut date)"); 
             JOptionPane.showMessageDialog(null, "Table Succesfully Created");
         }
         catch(Exception e)
